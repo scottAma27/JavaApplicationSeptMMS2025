86d19cde0f294a3292844ce2540fa876f003c32c
scottAma27 <scottma9@gmail.com>
2026-05-06 22:37:01 +0100

    this is a Assignment2

---
+++ Assignment1.java
@@ -0,0 +1,63 @@
+public static void main (String [] args){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
+public static void main (String [] agrs){
+	
+}
