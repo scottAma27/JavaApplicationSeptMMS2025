3ad68267ed86a3e9fff1800a73c00b759674f8df
scottAma27 <scottma9@gmail.com>
2026-05-25 15:40:00 +0100

    Assignment

--- Chapter 3/DaysOfTheWeek.java
+++ Chapter 3/DaysOfTheWeek.java
@@ -7,7 +7,39 @@ public class DaysOfTheWeek{
 	int day;
 	
 			System.out.printf("Enter day from 1 - 7: ");
-			int day = scan.nextInt();
+			day = input.nextInt();
 			
+		switch(day){
+			case 1:
+				System.out.println("Sunday");
+				break;
+				
+			case 2:
+				System.out.println("Monday");
+				break;
+				
+			case 3:
+				System.out.println("Tuesday");
+				break;
+				
+			case 4:
+				System.out.println("Wenesday");
+				break;
+				
+			case 5:
+				System.out.println("Thursday");
+				break;
+				
+			case 6:
+				System.out.println("Friday");
+				break;
+				
+			case 7:
+				System.out.println("Saturday");
+				break;
+				
+			default:
+			System.out.println("Invalid input");
+		}	
 	}
 }
